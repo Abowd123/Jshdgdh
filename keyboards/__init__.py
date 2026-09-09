3a555fe80ef6ff97a627880766bb9c8e5d10c738
Abowd123 <163924398+Abowd123@users.noreply.github.com>
2026-09-09 07:55:56 +0300

    upload keyboards/__init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,25 @@
+from .main_menu import MainMenu
+from .series_browse import SeriesBrowse
+from .series_detail import SeriesDetail
+from .episode_player import EpisodePlayer
+from .admin_panel import AdminPanel
+from .confirmations import Confirmations
+from .notifications import NotificationSystem
+from .advanced_search import AdvancedSearch
+from .rating_system import RatingSystem
+from .watchlist import Watchlist
+from .reply_kb import QuickMenu
+
+__all__ = [
+    "MainMenu",
+    "SeriesBrowse",
+    "SeriesDetail",
+    "EpisodePlayer",
+    "AdminPanel",
+    "Confirmations",
+    "NotificationSystem",
+    "AdvancedSearch",
+    "RatingSystem",
+    "Watchlist",
+    "QuickMenu",
+]
